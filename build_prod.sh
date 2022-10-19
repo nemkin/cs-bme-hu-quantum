02025a2aeef42a8329e8173fa9fda4b3c4aad4be
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2022-10-19 16:45:05 +0200

    New release

--- build_prod.sh
+++
@@ -1,8 +0,0 @@
-bundle exec jekyll build --config=_config.yml,_config.prod.yml
-cd ../szit-quantum-deploy
-git pull
-rm -rf *
-cp -r ../szit-quantum/_site/* .
-git add -A
-git commit -m "New release"
-git push
